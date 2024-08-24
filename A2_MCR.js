fbf7141433e2017ee5ed5060b20ad9c14e3404b9
Lakhan Raut <92510591+lakhanraut24@users.noreply.github.com>
2024-08-24 07:20:23 +0000

    I am Done First Machine Coding Round that is A2_MCR Successfully Completed

---
+++ A2_MCR.js
@@ -0,0 +1,60 @@
+console.log('A2_MCR')
+
+console.log('---- ---- ----')
+
+// variable:
+let freelancer1 = 'Sneha'
+let freelancer2 = 'Vikram'
+
+// job Description:
+let jobDescription1 = 'App Developer'
+let jobDescription2 = 'SEO Specialist'
+
+// Hourly Rates:
+let hourlyRate1 = 100
+let hourlyRate2 = 350
+
+// Hours Worked:
+let hoursWorked1 = 78
+let hoursWorked2 = 45
+
+// Ratings:
+let ratings1 = 88
+let ratings2 = 78
+
+// Eligibility for Bonus:
+let isEligibleForBonus1 = hoursWorked1 > 50 || ratings1 > 80 
+
+let isEligibleForBonus2 = hoursWorked2 > 50 || ratings2 > 80
+
+// calculate :
+
+totalPayment1 = hourlyRate1 * hoursWorked1
+
+totalPayment2 = hourlyRate1 * hoursWorked1
+
+console.log('Payment Details:')
+
+console.log('--------------------------')
+
+// Freelancer - 1
+console.log('Freelancer:',freelancer1)
+console.log('job Description:',jobDescription1)
+console.log('Hourly Rate: ₹', hourlyRate1)
+console.log('Hours Worked:', hoursWorked1)
+console.log('Total Payment: ₹', totalPayment1 )
+console.log('Ratings:', ratings1)
+console.log('Is Eligible for Bonus: ', isEligibleForBonus1)
+
+console.log('--------------------------')
+// Freelancer - 2 
+
+console.log('Freelancer:',freelancer2)
+console.log('job Description:',jobDescription2)
+console.log('Hourly Rate: ₹', hourlyRate2)
+console.log('Hours Worked:', hoursWorked2)
+console.log('Total Payment: ₹', totalPayment2 )
+console.log('Ratings:', ratings2)
+console.log('Is Eligible for Bonus: ', isEligibleForBonus2)
+
+
